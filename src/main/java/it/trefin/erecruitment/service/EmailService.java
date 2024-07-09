37c1c40abbc4e41910917714c1b2c0aaa287c356
ChristianMascolo <mascolokris@gmail.com>
2024-07-09 12:54:53 +0200

    fix email

--- src/main/java/it/trefin/erecruitment/service/EmailService.java
+++ src/main/java/it/trefin/erecruitment/service/EmailService.java
@@ -1,7 +1,5 @@
 package it.trefin.erecruitment.service;
 
-import java.util.Properties;
-
 import javax.transaction.Transactional;
 
 import org.slf4j.Logger;
@@ -10,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 import it.trefin.erecruitment.model.Response.Status;
 import it.trefin.erecruitment.model.Response;
@@ -21,7 +18,7 @@ public class EmailService {
 	private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
 
 	@Autowired
-	private JavaMailSender emailSender;
+	private JavaMailSender javaMailSender;
 
 	@Value("${spring.mail.from}")
 	private String from;
@@ -29,25 +26,23 @@ public class EmailService {
 	public Response<SimpleMailMessage, Status> inviaEmail(String[] destinatario, String oggetto, String testo) {
 		SimpleMailMessage msg = new SimpleMailMessage();
 		Response<SimpleMailMessage, Status> response = new Response<>();
-
 		try {
 			msg.setTo(destinatario);
 			msg.setFrom(from);
 			msg.setCc("christian.mascolo.3fedin@hotmail.com");
 			msg.setSubject(oggetto);
 			msg.setText(testo);
-			emailSender.send(msg);
 
+			javaMailSender.send(msg);
 			response.setData(msg);
 			response.setStatus(Status.OK);
-			response.setDescrizione("Email inviata con successo");
+			response.setDescrizione("Email inviata con successo ");
 			return response;
+
 		} catch (Exception e) {
-			logger.error("Error sending email", e);
-			// response.setData(msg);
+			logger.error("Errore durante l'invio dell'email: {}", e.getMessage());
 			response.setStatus(Status.SYSTEM_ERROR);
-			response.setDescrizione("Error sending email: " + e.getMessage());
-			System.out.println(response.getDescrizione());
+			response.setDescrizione("Errore durante l'invio dell'email: " + e.getMessage());
 			return response;
 		}
 	}

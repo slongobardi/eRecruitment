aa41b8164d9033de4c2c0160ae873e476e2743d1
AlessioSalzillo <alessiosalzillo2107@icloud.com>
2024-08-06 12:31:05 +0200

    be

--- src/main/java/it/trefin/erecruitment/repository/ColloquioRepository.java
+++ src/main/java/it/trefin/erecruitment/repository/ColloquioRepository.java
@@ -21,10 +21,18 @@ public interface ColloquioRepository extends JpaRepository<Colloquio, Long> {
 
 	ArrayList<Colloquio> findByUltimoColloquioBetween(Date start, Date end);
 
-	@Query(value = "SELECT COUNT(colloquio.id),SUM(colloquio.feedback =0),SUM(colloquio.feedback = 1)" + "FROM "
-			+ "colloquio,candidatura " + "WHERE "
-			+ "colloquio.id_candidatura = id_candidatura && id_candidatura = candidatura.id && candidatura.id_azienda = :id && "
-			+ "colloquio.ultimo_colloquio BETWEEN :start AND :end  ;", nativeQuery = true)
-	Object[] totalFeedback(@Param("id")int  id,@Param("start") Date startDate,@Param("end") Date endDate);
+	@Query(value = 
+			" SELECT COUNT(colloquio.id),SUM(colloquio.feedback =1),SUM(colloquio.feedback = 0),SUM(colloquio.feedback =2),SUM(schedacandidato.picking = 1),SUM(schedacandidato.perso = 1),SUM(schedacandidato.ingaggiato = 1)"
+			+ "FROM colloquio "
+			+"JOIN \r\n"
+			+ "    candidatura ON colloquio.id_candidatura = candidatura.id\r\n"
+			+ "JOIN \r\n"
+			+ "    utente_colloquio ON utente_colloquio.id_colloquio = colloquio.id\r\n"
+			+ "JOIN \r\n"
+			+ "    utente ON utente.id = utente_colloquio.id_utente\r\n"
+			+ "JOIN \r\n"
+			+ "    schedacandidato ON schedacandidato.id = utente.id_scheda_candidato\r\n" 
+			+ "WHERE candidatura.id_azienda = :id && colloquio.ultimo_colloquio BETWEEN :start AND :end ;", nativeQuery = true)
+	Object[] totalFeedback(@Param("id") int id, @Param("start") Date startDate, @Param("end") Date endDate);
 
 }
